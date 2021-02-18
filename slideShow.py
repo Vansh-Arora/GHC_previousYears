62d3af2d50303d542ccba1137665a910e9d9f562
Vansh <vansharora2929@gmail.com>
2021-02-18 12:22:38 +0530

    dictionaries made

--- slideShow.py
+++ slideShow.py
@@ -1,2 +1,10 @@
 inputFile = open("F:\\GHC_previousYears\\b_lovely_landscapes.txt","r")
 lines = inputFile.readline()
+dic1 = {}
+dic2 = {}
+for i in range(3):
+    myList = (inputFile.readline()).split()
+    dic1[i] = myList[1]
+    dic2[i] = myList[1:]
+print(dic1)
+print(dic2)
