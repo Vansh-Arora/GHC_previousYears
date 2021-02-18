311a3aaf2a7cdcd3d6636f8453218cb6e62c6723
Vansh <vansharora2929@gmail.com>
2021-02-18 16:48:30 +0530

    changes

--- slideShow.py
+++ slideShow.py
@@ -1,10 +1,14 @@
 inputFile = open("F:\\GHC_previousYears\\b_lovely_landscapes.txt","r")
-lines = inputFile.readline()
+outputFile = open("F:\\GHC_previousYears\\sol.txt","w")
+outputFile2 = open("F:\\GHC_previousYears\\sol2.txt","w")
+
+lines = int(inputFile.readline())
 dic1 = {}
 dic2 = {}
-for i in range(3):
+for i in range(lines):
     myList = (inputFile.readline()).split()
-    dic1[i] = myList[1]
-    dic2[i] = myList[1:]
-print(dic1)
-print(dic2)
+    dic1[i] = int(myList[1])
+    dic2[i] = myList[2:]
+for i in range(lines):
+    outputFile.write(str(dic1[i]) + " " + str(i) + "\n")
+    outputFile2.write(str(dic2[i]) + "\n") 
